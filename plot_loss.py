de85f9b4b9e7a7fd838944d48a32627708c1184f
Ahmedsalaheld <shazaahmedd@icloud.com>
2020-12-05 00:28:25 +0200

    losses

--- plot_loss.py
+++ plot_loss.py
@@ -16,11 +16,12 @@ def parse_args():
 
 def main():
     args = parse_args()
-
+    # opens log file for reading
     with open(args.log_file, "r") as f:
         lines = f.readlines()
     real_losses = []
     fake_losses = []
+    # collects the data for both of the fake losses and the real losses
     for line in lines:
         line = line.strip()
         if line.startswith("fake_loss"):
@@ -29,11 +30,13 @@ def main():
             real_losses.append(float(line.split(" ")[-1]))
     real_losses = np.array(real_losses)
     fake_losses = np.array(fake_losses)
+    # computes the weighted loss
     loss = (fake_losses * args.fake_weight + real_losses)/2
     plt.title("Weighted loss ({}*fake_loss + real_loss)/2)".format(args.fake_weight))
     best_loss_idx = np.argsort(loss)[:5]
     # ignore early epochs  loss is quite noisy and there could be spikes
     best_loss_idx = best_loss_idx[best_loss_idx > 16]
+    # plot the best loss in terms of the weighted loss it just computed
     plt.scatter(best_loss_idx, loss[best_loss_idx], c="red")
     for idx in best_loss_idx:
         plt.annotate(str(idx), (idx, loss[idx]))
